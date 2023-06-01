03fe892a09dc183cc1aae746a7f7498717873229
WissalManseri <135167709+WissalManseri@users.noreply.github.com>
2023-06-01 13:26:03 +0100

    Add files via upload

---
+++ observerMain.java
@@ -0,0 +1,41 @@
+public class BinaryObserver implements Observer {
+    private Subject subject;
+
+    public BinaryObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Binary String: " + Integer.toBinaryString(subject.getState()));
+    }
+}
+
+public class OctalObserver implements Observer {
+    private Subject subject;
+
+    public OctalObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Octal String: " + Integer.toOctalString(subject.getState()));
+    }
+}
+
+public class HexObserver implements Observer {
+    private Subject subject;
+
+    public HexObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Hex String: " + Integer.toHexString(subject.getState()).toUpperCase());
+    }
+}
