781325722ae94a1d48f906924ca9db1b3137de1e
WissalManseri <135167709+WissalManseri@users.noreply.github.com>
2023-06-01 13:26:17 +0100

    Add files via upload

---
+++ Observer pattern .java
@@ -0,0 +1,76 @@
+/*The Observer pattern is a behavioral design pattern that allows objects to be notified when the state of another object changes. 
+In this pattern, an object, called the subject, maintains a list of its dependents, called observers, and notifies them automatically of any state changes, 
+usually by calling one of their methods.
+*/
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+    private List<Observer> observers = new ArrayList<>();
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObservers();
+    }
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
+/*Observer interface:*/
+public interface Observer {
+    void update();
+}
+/*Concrete observer classes:*/
+public class BinaryObserver implements Observer {
+    private Subject subject;
+
+    public BinaryObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Binary String: " + Integer.toBinaryString(subject.getState()));
+    }
+}
+
+public class OctalObserver implements Observer {
+    private Subject subject;
+
+    public OctalObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Octal String: " + Integer.toOctalString(subject.getState()));
+    }
+}
+
+public class HexObserver implements Observer {
+    private Subject subject;
+
+    public HexObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Hex String: " + Integer.toHexString(subject.getState()).toUpperCase());
+    }
+}
